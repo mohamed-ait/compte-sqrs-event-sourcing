c0876c7a57c45d32d2805a9865f37e38abf1b439
Mohamed Ait Lahcen <mohamedaitlahcen@gmail.com>
2022-12-25 21:54:27 +0100

    create AccountAggregate

--- src/main/java/org/sid/comptesqrseventsourcing/commands/aggregates/AccountAggregate.java
+++ src/main/java/org/sid/comptesqrseventsourcing/commands/aggregates/AccountAggregate.java
@@ -1,12 +1,14 @@
 package org.sid.comptesqrseventsourcing.commands.aggregates;
 
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
 import org.sid.comptesqrseventsourcing.commonApi.commands.CreateAccountCommand;
 import org.sid.comptesqrseventsourcing.commonApi.dtos.CreateAccountRequestDTO;
 import org.sid.comptesqrseventsourcing.commonApi.enums.AccountStatus;
+import org.sid.comptesqrseventsourcing.commonApi.events.AccountCreatedEvent;
 
 @Aggregate
 public class AccountAggregate {
@@ -22,7 +24,17 @@ public class AccountAggregate {
     @CommandHandler //pour écouter le bus
     public AccountAggregate(CreateAccountCommand command) {
         if(command.getInitialBalance()<0) throw  new RuntimeException("impossible de créer un compte avec un solde négative");
-        AggregateLifecycle.apply(new )
+        AggregateLifecycle.apply(new AccountCreatedEvent(
+                command.getId(),
+                command.getInitialBalance(),
+                command.getCurrency()
+        ));
     }
-
+    @EventSourcingHandler
+    public void on(AccountCreatedEvent event){
+        this.accountId= event.getId();
+        this.balance= event.getBalance();
+        this.status=AccountStatus.CREATED;
+        this.currency=event.getCurrency();
+    }
 }
--- src/main/java/org/sid/comptesqrseventsourcing/commonApi/events/AccountCreatedEvent.java
+++ src/main/java/org/sid/comptesqrseventsourcing/commonApi/events/AccountCreatedEvent.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 public class AccountCreatedEvent extends BaseEvent<String>{
     @Getter private double balance;
     @Getter private String currency;
-    public AccountCreatedEvent(String id) {
+    public AccountCreatedEvent(String id,double balance,String currency) {
         super(id);
+        this.balance=balance;
+        this.currency=currency;
     }
 }
