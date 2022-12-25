772852099eec5ff220dec312baaaf1144d2d7220
Mohamed Ait Lahcen <mohamedaitlahcen@gmail.com>
2022-12-25 19:21:10 +0100

    create commands

---
+++ src/main/java/org/sid/comptesqrseventsourcing/commonApi/commands/BaseCommand.java
@@ -0,0 +1,13 @@
+package org.sid.comptesqrseventsourcing.commonApi.commands;
+
+import lombok.Getter;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+public abstract class BaseCommand<T> {
+    @TargetAggregateIdentifier
+    @Getter private T id;
+
+    public BaseCommand(T id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/org/sid/comptesqrseventsourcing/commonApi/commands/CreditAccountCommand.java
@@ -0,0 +1,11 @@
+package org.sid.comptesqrseventsourcing.commonApi.commands;
+
+public class DebitAccountCommand extends BaseCommand<String>{
+    private double amount;
+    private String currency;
+    public DebitAccountCommand(String id, double amount, String currency) {
+        super(id);
+        this.amount=amount;
+        this.currency=currency;
+    }
+}
