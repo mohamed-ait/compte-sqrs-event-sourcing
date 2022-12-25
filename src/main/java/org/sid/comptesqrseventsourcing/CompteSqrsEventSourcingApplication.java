a839ac8cf612df9bc737a7fcf11122ec37a9b555
Mohamed Ait Lahcen <mohamedaitlahcen@gmail.com>
2022-12-25 18:59:00 +0100

    first commit

---
+++ src/main/java/org/sid/comptesqrseventsourcing/CompteSqrsEventSourcingApplication.java
@@ -0,0 +1,13 @@
+package org.sid.comptesqrseventsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompteSqrsEventSourcingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompteSqrsEventSourcingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/comptesqrseventsourcing/CompteSqrsEventSourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.comptesqrseventsourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompteSqrsEventSourcingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
