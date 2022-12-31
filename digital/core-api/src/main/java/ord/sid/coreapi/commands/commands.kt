0a3b3af32f5af5aaab417ac613fe6022df1f34d0
Mohamed Ait Lahcen <mohamedaitlahcen@gmail.com>
2022-12-31 03:20:38 +0100

    create customer commands

--- digital/core-api/src/main/java/ord/sid/coreapi/commands/commands.kt
+++ digital/core-api/src/main/java/ord/sid/coreapi/commands/commands.kt
@@ -1,2 +1,17 @@
 package ord.sid.coreapi.commands
 
+abstract class BaseCommand<T>(
+        open val  id : T
+)
+
+data class CreateCustomerCommand(
+        override val id :String,
+        val name :String,
+        val email : String
+):BaseCommand<String>(id)
+
+data class UpdateCustomerCommand(
+        override val id :String,
+        val name :String,
+        val email : String
+):BaseCommand<String>(id)
